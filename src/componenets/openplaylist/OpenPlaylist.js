4853eb9f10c4234d83ce47468dc8d1ec6e8b9399
dzitting <zittingcodes@gmail.com>
2023-08-17 14:31:52 -0600

    Users can remove songs from each playlist by clicking the x button. It will update the playlists, and selectedPlaylists state.

--- src/componenets/openplaylist/OpenPlaylist.js
+++ src/componenets/openplaylist/OpenPlaylist.js
@@ -16,19 +16,24 @@ function OpenPlaylist(props) {
     borderRadius: 12,
     height: "calc(100vh / 6)",
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
   };
 
   return (
     <>
-    {props.editingName ? (
-      <form onSubmit={props.confirmUpdate}>
-        <input onChange={props.updatingNameEdit} style={{transform: 'translateX(320%)'}} value={props.playlist.name}/>
-      </form>
-    ): (
-      <h1 style={{ textAlign: "center" }}>{props.playlist.name}</h1>
-    )}
+      {props.editingName ? (
+        <form onSubmit={props.confirmUpdate}>
+          <input
+            onChange={props.updatingNameEdit}
+            style={{ transform: "translateX(320%)" }}
+            value={props.playlist.name}
+          />
+        </form>
+      ) : (
+        <h1 style={{ textAlign: "center" }}>{props.playlist.name}</h1>
+      )}
       <div
         style={{
           textAlign: "center",
@@ -41,28 +46,38 @@ function OpenPlaylist(props) {
         }}
       >
         {props.playlist.items.length < 1 ? (
-          <p style={{ textAlign: "center", width: "100%" }}>There are no songs in this playlist</p>
+          <p style={{ textAlign: "center", width: "100%" }}>
+            There are no songs in this playlist
+          </p>
         ) : (
           props.playlist.items.map((track) => (
-            <div style={cardStyle}>
-              <p>
+            <div style={cardStyle} key={`track-card-${track.id}`}>
+              <p id={`${track.id}`}>
                 {track.name} by {track.artist} from {track.album}
               </p>
+              <button onClick={props.removeSong}
+                style={{
+                  border: "none",
+                  background: "gray",
+                  cursor: "pointer",
+                  padding: "5px 10px",
+                  borderRadius: "5px",
+                  color: "white",
+                }}
+              >
+                X
+              </button>
             </div>
           ))
         )}
       </div>
-      <div style={{ width: "100vw", display: "flex", justifyContent: "center" }}>
-        <button
-          style={{ margin: "0 10px" }}
-          onClick={props.open}
-        >
+      <div
+        style={{ width: "100vw", display: "flex", justifyContent: "center" }}
+      >
+        <button style={{ margin: "0 10px" }} onClick={props.open}>
           Back
         </button>
-        <button onClick={props.changePlaylistName}
-        >
-          Edit Name
-        </button>
+        <button onClick={props.changePlaylistName}>Edit Name</button>
       </div>
     </>
   );
