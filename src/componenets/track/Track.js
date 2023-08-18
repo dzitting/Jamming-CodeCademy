d5ceaf48f033f5dd8e38b3fc81115000b8ddde7b
dzitting <zittingcodes@gmail.com>
2023-08-17 19:48:00 -0600

    Setup API call from mockaroo API false call. Spotify API was having issues on return

--- src/componenets/track/Track.js
+++ src/componenets/track/Track.js
@@ -4,7 +4,7 @@ import styles from "./Track.module.css";
 function Track({ track, index, handleAddSong}) {
   return (
     <div id={`track-${index}`} className={styles.div}>
-      <h1 className={styles.name}>{track.name}</h1>
+      <h1 className={styles.name}>{track.track_name}</h1>
       <h1 className={styles.artist}>{track.artist}</h1>
       <h1 className={styles.album}>{track.album}</h1>
       <button onClick={(e) => handleAddSong(e, index)}>Add to Playlist</button>
